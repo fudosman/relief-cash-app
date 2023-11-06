bf0afa175d33bb138aaca83e8318862ddb08df3d
fudosman <fudosman@gmail.com>
2023-11-06 17:39:18 +0100

    feat: update loan completed

--- src/configs/env.config.js
+++ src/configs/env.config.js
@@ -2,12 +2,6 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT,
-    email: process.env.EMAIL,
-    password: process.env.PASSWORD,
-    firstName: process.env.FIRST_NAME,
-    lastName: process.env.LAST_NAME,
-    phoneNumber: process.env.PHONE_NUMBER,
-    homeAddress: process.env.HOME_ADDRESS,
     dbUrl: process.env.MONGO_URL,
     accountSid: process.env.ACCOUNT_SID,
     apiKeySecret: process.env.AUTHENTICATION_TOKEN,
